5ed91737bbb2ff500599ae4d5024d71e424a21d6
alexpolyarny <alexpolyarny@gmail.com>
2026-04-30 11:52:38 +0300

    сode style changes to hello.py

--- lab01/hello.py
+++ lab01/hello.py
@@ -6,7 +6,9 @@ import typer
 def main(
     name: str,
     lastname: str = typer.Option("", help="Фамилия пользователя."),
-    formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
+    formal: bool = typer.Option(
+        False, "--formal", "-f", help="Использовать формальное приветствие."
+    ),
 ):
  """
  Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
@@ -15,6 +17,7 @@ def main(
         print(f"Добрый день, {name} {lastname}!")
     else:
         print(f"Привет, {name}!")
+
 
 if __name__ == "__main__":
     typer.run(main)
